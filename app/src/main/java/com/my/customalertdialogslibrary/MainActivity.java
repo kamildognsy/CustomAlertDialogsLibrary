ac95f988c045763699b4e89944005428383415da
KamilDogan <kamil.dogansoy@vbt.com.tr>
2022-03-11 00:13:26 +0300

    with one button and two button and three button show function added

--- app/src/main/java/com/my/customalertdialogslibrary/MainActivity.java
+++ app/src/main/java/com/my/customalertdialogslibrary/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.my.customalertlib.CustomAlertDialogs;
+
 public class MainActivity extends AppCompatActivity {
+    CustomAlertDialogs alert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        alert = new CustomAlertDialogs(MainActivity.this);
+        alert.showThreeButtonAlertDialog();
     }
 }
--- customalertlib/src/main/java/com/my/customalertlib/CustomAlertDialogs.java
+++ customalertlib/src/main/java/com/my/customalertlib/CustomAlertDialogs.java
@@ -8,45 +8,68 @@ import android.widget.TextView;
 
 public class CustomAlertDialogs {
     private Context context;
-    private String title;
-    private String btnPosiviteText;
-    private String btnNegativeText;
+    private Dialog dialog;
+    private Button btn_ok;
+    private Button btn_positive;
+    private Button btn_negative;
+    private Button btn_other;
 
-    public CustomAlertDialogs(Context context, String title, String btnPosiviteText, String btnNegativeText) {
+    public CustomAlertDialogs(Context context) {
         this.context = context;
-        this.title = title;
-        this.btnPosiviteText = btnPosiviteText;
-        this.btnNegativeText = btnNegativeText;
+        dialog = new Dialog(context);
     }
 
-    public void showMyCustomAlertDialog() {
-
-        final Dialog dialog = new Dialog(context);
-        dialog.setContentView(R.layout.custom_alert_dialog_two_button);
-
-        Button btn_positive = (Button) dialog.findViewById(R.id.btn_positive);
-        Button btn_negative = (Button) dialog.findViewById(R.id.btn_negative);
-        TextView tv_title = (TextView) dialog.findViewById(R.id.tv_title);
-
-        tv_title.setText(title);
-        btn_positive.setText(btnPosiviteText);
-        btn_negative.setText(btnNegativeText);
-
-        btn_positive.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialog.dismiss();
-            }
-        });
-
-        btn_negative.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialog.dismiss();
-            }
-        });
-
+    public void showOneButtonAlertDialog() {
+        dialog.setContentView(R.layout.custom_alert_dialog_one_button);
+        init();
         dialog.show();
+        clickListener(btn_ok);
     }
+
+    public void showTwoButtonAlertDialog() {
+        dialog.setContentView(R.layout.custom_alert_dialog_two_button);
+        init();
+        dialog.show();
+        clickListener(btn_positive);
+        clickListener(btn_negative);
+    }
+
+    public void showThreeButtonAlertDialog() {
+        dialog.setContentView(R.layout.custom_alert_dialog_three_button);
+        init();
+        dialog.show();
+        clickListener(btn_positive);
+        clickListener(btn_negative);
+        clickListener(btn_other);
+    }
+
+
+    private void init() {
+        TextView tv_title = dialog.findViewById(R.id.tv_title);
+        TextView tv_content = dialog.findViewById(R.id.tv_content);
+        btn_ok = dialog.findViewById(R.id.btn_ok);
+        btn_positive = dialog.findViewById(R.id.btn_positive);
+        btn_negative = dialog.findViewById(R.id.btn_negative);
+        btn_other = dialog.findViewById(R.id.btn_other);
+
+    }
+
+    private void clickListener(View v) {
+        v.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (view.getId() == R.id.btn_ok) {
+                    System.out.println("btn ok");
+                } else if (view.getId() == R.id.btn_positive) {
+                    System.out.println("btn positive");
+                } else if (view.getId() == R.id.btn_negative) {
+                    System.out.println("btn negative");
+                } else {
+                    System.out.println("btn other");
+                }
+            }
+        });
+    }
+
 
 }
